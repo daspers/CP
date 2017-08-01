7c732c507fe313faa3cc9f9a89ac4738abd61941
daspers <buddy90_lost@yahoo.co.id>
2017-08-01 22:20:07 +0700

    Create Pola I.cpp

---
+++ Toki/Pemrograman Dasar/06 Perulangan/Pola I.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x,p;
+	cin >>x>>p;
+	for(int i=1;i<=x;i++){
+		if(i%p>0)
+      cout<<i;
+		else
+      cout<<'*'; 
+		if(i!=x)
+      cout<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
