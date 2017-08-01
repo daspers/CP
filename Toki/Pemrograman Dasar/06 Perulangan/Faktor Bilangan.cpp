e2eaa87b669d6aaedf12dafa643a2f67432c4607
daspers <buddy90_lost@yahoo.co.id>
2017-08-01 22:17:49 +0700

    Create Faktor Bilangan.cpp

---
+++ Toki/Pemrograman Dasar/06 Perulangan/Faktor Bilangan.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	unsigned long x;
+	cin >>x;
+	for(int i=x;i>0;i--){
+		if (x%i<1)
+      cout<<i<<endl;
+	}
+	return 0;
+}
