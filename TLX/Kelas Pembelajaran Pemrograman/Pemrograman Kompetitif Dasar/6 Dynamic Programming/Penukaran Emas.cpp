12c76d45f802a3afde9991315fe1beb82865b035
Tony <buddy90_lost@yahoo.co.id>
2017-11-14 07:25:09 +0700

    Create Penukaran Emas.cpp

---
+++ TLX/Kelas Pembelajaran Pemrograman/Pemrograman Kompetitif Dasar/6 Dynamic Programming/Penukaran Emas.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, dp[1003];
+
+int berat(int a){
+	if(dp[a]==-1)
+		dp[a] = max(a/4, berat(a/4))+max(a/3, berat(a/3))+max(a/2, berat(a/2));
+	return dp[a];
+}
+
+int main(){
+	scanf("%d", &n);
+	memset(dp, -1, sizeof(dp));
+	dp[0] = 0;
+	printf("%d\n", berat(n));
+	return 0;
+}
