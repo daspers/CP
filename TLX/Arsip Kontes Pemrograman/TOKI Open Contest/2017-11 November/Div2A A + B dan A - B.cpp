a1b601c2d1e30a1f7bd757024aca9876a89502f1
Tony <buddy90_lost@yahoo.co.id>
2018-01-12 19:10:23 +0700

    Create Div2A A + B dan A - B.cpp

---
+++ TLX/Arsip Kontes Pemrograman/TOKI Open Contest/2017-11 November/Div2A A + B dan A - B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n,i,j,k,t,mi;
+ull a,b;
+
+int main(){
+	puts("1 2");
+	a = b = pow(2,63);
+	printf("%llu %llu\n", a, b);
+	return 0;
+}
