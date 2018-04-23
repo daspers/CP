e0d00ff0c8fafbb0207ba160ce4c7f9d76bcb2b2
Tony <buddy90_lost@yahoo.co.id>
2018-04-23 16:28:46 +0700

    add soal dan sedikit solusi final

--- Vocompfest/2018/Final/e.cpp
+++ Vocompfest/2018/Final/e.cpp
@@ -1,8 +1,3 @@
-/*
-	WARNING!!!
-	WRONG SOLUTION
-*/
-
 #include <bits/stdc++.h>
  
 using namespace std;
@@ -11,30 +6,20 @@ using namespace std;
 #define se second
 #define mp make_pair
 #define pb push_back
+#define baca getchar_unlocked
 
 typedef long long ll;
-
-const ll mod = 1e9+7;
+ 
+const ll mod = 1e9+9;
 const double pi = acos(-1);
-
-int n,t,i,j,k;
-string s;
-int a[101], c, tot;
-int b[101];
+const int maxsz = 10;
 
 int main(){
+	int t;
 	scanf("%d", &t);
-	for(int cc=1;t--;cc++){
+	for(int cc=1;cc<=t;++cc){
 		printf("Case #%d: ", cc);
-		cin>>s;
-		s = s;
-		a[0]=0;
-		tot = 0;
-		for(i=0;i<s.size();i++){
-			
-		}
-		while(
-			
+		
 	}
 	return 0;
 }
