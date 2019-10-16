c3ecbea1209e1699b8d06e84f163750cec280b18
Tony <buddy90_lost@yahoo.co.id>
2019-10-16 08:27:19 +0700

    update template

--- template.cpp
+++ template.cpp
@@ -9,6 +9,10 @@ using namespace std;
 #define INF 2000000000000000000LL
 #define EPS 1e-9
 #define debug(a) cerr<<#a<<"="<<(a)<<"\n"
+#define debug2(a, b) cerr<<#a<<"="<<(a)<<" ";debug(b)
+#define debug3(a, b, c) cerr<<#a<<"="<<(a)<<" ";debug2(b, c)
+#define debug4(a, b, c, d) cerr<<#a<<"="<<(a)<<" ";debug3(b, c, d)
+#define debug5(a, b, c, d, e) cerr<<#a<<"="<<(a)<<" ";debug4(b, c, d, e)
 #define FastSlowInput ios_base::sync_with_stdio(false); cin.tie(NULL);
 
 typedef long long ll;
@@ -17,8 +21,10 @@ typedef complex<double> cd;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-void printArray(const int *arr, int n);
-void printArray(const vector<int> &arr);
+template<class T>
+void printArray(const T * a, int n, ostream& out = cerr);
+template<class T>
+void printArray(const vector<T> &arr, ostream& out = cerr);
 
 const ll mod = 1e9+7;
 const double PI = acos(-1);
@@ -32,16 +38,18 @@ int main(){
 }
 
 /* Template Function */
-void printArray(const int * a, int n){
+template<class T>
+void printArray(const T * a, int n, ostream& out){
 	for(int i=0;i<n;++i){
-		printf("%d ", a[i]);
+		out<<a[i]<<" ";
 	}
-	puts("");
+	out<<endl;
 }
 
-void printArray(const vector<int> &arr){
-	for(int x : arr){
-		printf("%d ", x);
+template<class T>
+void printArray(const vector<T> &arr, ostream& out){
+	for(const T& x : arr){
+		out<<x<<" ";
 	}
-	puts("");
+	out<<endl;
 }
